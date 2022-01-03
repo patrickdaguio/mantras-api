8ec9abd368c689bef38cc08532357665619331b2
Patrick Daguio <patrickdaguio@outlook.com>
2022-01-03 07:49:05 +0000

    Added middleware for localhost

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/mantras', (req, res) => {
 	res.send(db);
 });
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
