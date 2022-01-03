9a650f5e9572971a847eebcb2f01a9d2ae1c51bb
Patrick Daguio <patrickdaguio@outlook.com>
2022-01-03 08:00:17 +0000

    Added cors module

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const db = require('./db.json');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/mantras', (req, res) => {
 	res.send(db);
 });
 
-app.use(function (req, res, next) {
+/* app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept'
 	);
 	next();
-});
+}); */
 
 const port = process.env.PORT || 3000;
 
